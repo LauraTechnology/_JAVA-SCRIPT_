9dca28a9dabed824e468cecb6aa187a0d156e1ac
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-07-15 13:40:31 +0300

    pradžia

---
+++ vidurkis.js
@@ -0,0 +1,42 @@
+// cia yra ne komanda, o tekstas, kuri programutoojas pasirase sau. 
+// - single line komentaras (vinea eilute) 
+var a;
+var b;
+var c; // var - komanda Java Scriptui. Fiksuotas terminas, variable-kintamasis. Deklaruojam kintamaji
+
+//kaip galima vadinti kintamuosiuos? 
+/* siuo atveju vidurkis yra kintamasis
+
+galima naudoti bet kurias raides (ir simbolius)
+
+negali prasideti skaiciumi
+negali buti tarpu
+
+iprasta naudoti 'camel-case'
+
+*/
+
+//Java Script yra Case sensitive
+
+*/
+
+var bendrasasakitossuma;
+var bendra_saskaitos_suma; //snake case
+var bendraSasakitosSuma; - //camel case
+
+
+a = 2;
+b = 7;
+c = 9;
+
+var suma;
+
+suma = a /* cia kintamasis a */ + b /* cia kintamasis b */ + c;
+
+var vidurkis;
+
+vidurkis = suma / 3;
+
+console.log(viduris);
+
+
