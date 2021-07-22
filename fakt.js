b9bb0be57059ddb3284589b5b5e8b85f60fff27c
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-07-22 12:26:01 +0300

    faktorialas

--- fakt.js
+++ fakt.js
@@ -1,1 +1,10 @@
-var sk = 0;
+var sk = 4;
+if (sk >= 0) {
+var f = 1;
+for (var i = 2; i <= sk; i++) {
+f = f * i;
+}
+console.log(f);
+} else {
+console.log("Negalima paskaiciuoti neigiamo skaiciaus faktorialo");
+}
