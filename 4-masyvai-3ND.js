a25f7f8af448546f880dbba3e533928dda9b7ccd
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-07-27 12:29:53 +0300

    Namu Darbai Masinu-lenktynes

--- 4-masyvai-3ND.js
+++ 4-masyvai-3ND.js
@@ -1,1 +1,27 @@
 /* Masinu lenktynes */
+const masinos = [0, 0, 0, 0, 0, 0, 0, 0];
+let vaziuojam = true;
+// lenktyniu ciklas
+while (vaziuojam) {
+// visos masinos pavaziavo
+for(let i = 0; i < masinos.length; i++) {
+masinos[i] += Math.trunc(Math.random() * 5 + 1);
+}
+// ieskom ar yra finisavusiu
+for(let i = 0; i < masinos.length; i++) {
+if (masinos[i] >= 1000) {
+vaziuojam = false;
+break;
+}
+}
+}
+// kuri masina laimejo
+let laimetojas = 0;
+for (let i = 1; i < masinos.length; i++) {
+if (masinos[i] > masinos[laimetojas]) {
+laimetojas = i;
+}
+}
+// spausdinam laimetoja
+console.log(masinos);
+console.log(laimetojas, masinos[laimetojas]);
