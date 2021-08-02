7c9638fe322d17e5fae4e1251cd6bfca02434bc2
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-02 09:50:07 +0300

    Kaip sukurti logaritma,kuris parodytu pirminius sk

---
+++ pirminiai.js
@@ -0,0 +1,6 @@
+/*
+surasti visus pirminius skaicius intervale 2 iki 100
+
+Sprendimo zingsniai:
+1.reiks perziureti visus skaicius nuo 2 iki 100
+2. kiekviena skaicu reikia dalinti nuo 2 iki jo paties (ne imtinai)
