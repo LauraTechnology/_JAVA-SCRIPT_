88ce5eafa51f6fc3acc3e6a6b0f41a5eda06949f
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-02 10:42:55 +0300

    Smulkesnis sprendimo zingsniu aptarimas

--- pirminiai.js
+++ pirminiai.js
@@ -2,9 +2,15 @@
 surasti visus pirminius skaicius intervale 2 iki 100
 
 Sprendimo zingsniai:
-reiks perziureti visus skaicius nuo 2 iki 100
+reiks perziureti visus skaicius nuo 2 iki 100 
+- cia reiciausiai panaudosim cikla
+
 kiekviena skaicu reikia dalinti nuo 2 iki jo paties (ne imtinai)
+-viduj reikes dar karta tikrinti, nuo to skaiciaus ziureti visus mazesnius
+
 dalinant tikrinti ar dalinasi be liekanos
+-TIKRINTI AR NERA DALIKLIU, KURIE DALINA BE LIEKANOS
+
 jei be liekanos - ne pirminis
 jei nera nei vieno skaiciaus, is kurio padalinus tikrinama skaiciu, gautume liekana 0,
 tai tikrinamas skaicius - pirminis
