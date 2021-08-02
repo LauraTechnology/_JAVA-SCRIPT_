ebd8c43ebdb65e2f63d12bf8edbb033999d778d3
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-02 09:50:27 +0300

    Logaritmas pirminiams skaiciams

--- pirminiai.js
+++ pirminiai.js
@@ -2,5 +2,9 @@
 surasti visus pirminius skaicius intervale 2 iki 100
 
 Sprendimo zingsniai:
-1.reiks perziureti visus skaicius nuo 2 iki 100
-2. kiekviena skaicu reikia dalinti nuo 2 iki jo paties (ne imtinai)
+reiks perziureti visus skaicius nuo 2 iki 100
+kiekviena skaicu reikia dalinti nuo 2 iki jo paties (ne imtinai)
+dalinant tikrinti ar dalinasi be liekanos
+jei be liekanos - ne pirminis
+jei nera nei vieno skaiciaus, is kurio padalinus tikrinama skaiciu, gautume liekana 0,
+tai tikrinamas skaicius - pirminis
