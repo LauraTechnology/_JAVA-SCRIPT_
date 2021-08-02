5011973c200f14288d90dfd9db25e964418c8fb9
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-02 12:18:00 +0300

    Procesas Puodukas

---
+++ paprastosklases.js
@@ -0,0 +1,9 @@
+function Puodukas(spalva, turis) {
+  this.spalva = spalva;
+  this.turis = turis;
+  this.kiekis = 0;
+}
+let p1 = new Puodukas("rudas", 250);
+let p2 = new Puodukas("baltas", 150);
+console.log(p1);
+console.log(p2);
