9309b38f7e3c5772b77f253268ff44392dcff5dc
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-04 14:46:10 +0300

    event

---
+++ event.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+console.log("pradzia");
+fs.readFile(
+  "a.txt",
+  {
+    encoding: "utf-8",
+  },
+  function (err, data) {
+    if (err) {
+      console.log("nepavyko perskaityti failo");
+      console.log(err);
+      return;
+    }
+    console.log("perskaiciau faila");
+    console.log(data);
+  }
+);
+setTimeout(function () {
+  console.log("dirbu");
+}, 10);
+console.log("pabaiga");
