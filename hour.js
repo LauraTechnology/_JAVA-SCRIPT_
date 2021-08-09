624f03b28e659bd6f271301fafe901d5d2dc39d3
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-09 09:52:25 +0300

    pataisytas hours.js (24h)

--- hour.js
+++ hour.js
@@ -8,11 +8,8 @@ let hour = 10;
 if (hour >= 6 && hour < 12) {
     console.log('Good morning');
 }
-else if (hour >= 12 && hour < 6) {
+else if (hour >= 12 && hour < 18) {
     console.log('Good afternoon');
 }
-else if (hour > 6 && hour < 6 ) {
-    console.log('Good evening');
-}
 else
-    statement
+    console.log('Good evening');
