dd61244ae4f04101b3ba010b1404bf21395d438b
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-09 12:01:18 +0300

    fukncijos-ivairiod

---
+++ funkcijos-ivairios.js
@@ -0,0 +1,9 @@
+let labas = function (vardas, pavarde) { /*turi prototipa */
+  console.log("Labas", vardas, pavarde);
+  // return undefined;
+};
+
+let ate = (vardas, pavarde) => { /* neturi prototipo */
+  console.log("Ate", vardas, pavarde);
+  // return undefined;
+}; 
